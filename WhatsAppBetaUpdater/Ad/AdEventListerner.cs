674b90deab08bc891279d8324a1cfb178e271ee8
Javier Santos <javier.santos.vel@gmail.com>
2015-04-19 20:54:29 +0200

    Update Ads

---
+++ WhatsAppBetaUpdater/Ad/AdEventListerner.cs
@@ -0,0 +1,34 @@
+﻿
+using Android.OS;
+using Android.Gms.Ads;
+
+namespace WhatsAppBetaUpdater {
+	class AdEventListerner : AdListener {
+		// Declare the delegate (if using non-generic pattern). 
+		public delegate void AdLoadedEvent();
+		public delegate void AdClosedEvent();
+		public delegate void AdOpenedEvent();
+
+		// Declare the event. 
+		public event AdLoadedEvent AdLoaded;
+		public event AdClosedEvent AdClosed;
+		public event AdOpenedEvent AdOpened;
+
+		public override void OnAdLoaded() {
+			if (AdLoaded != null) this.AdLoaded();
+			base.OnAdLoaded();
+		}
+
+		public override void OnAdClosed() {
+			if (AdClosed != null) this.AdClosed();
+			base.OnAdClosed();
+		}
+
+		public override void OnAdOpened() {
+			if (AdOpened != null) this.AdOpened();
+			base.OnAdOpened();
+		}
+
+	}
+}
+
---
+++ WhatsAppBetaUpdater/Ad/AdWrapper.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Gms.Ads;
+
+namespace WhatsAppBetaUpdater {	
+	public static class AdWrapper {
+		public static InterstitialAd ConstructFullPageAdd(Context con, string UnitID)
+		{
+			var ad = new InterstitialAd(con);
+			ad.AdUnitId = UnitID;
+			return ad;
+		}
+
+		public static AdView ConstructStandardBanner(Context con, AdSize adSize, string UnitID)
+		{
+			var ad = new AdView(con);
+			ad.AdSize = adSize;
+			ad.AdUnitId = UnitID;
+			return ad;
+		}
+
+		public static InterstitialAd CustomBuild(this InterstitialAd ad)
+		{
+			var requestbuilder = new AdRequest.Builder();
+			ad.LoadAd(requestbuilder.Build());
+			return ad;
+		}
+
+		public static AdView CustomBuild(this AdView ad)
+		{
+			var requestbuilder = new AdRequest.Builder();
+			ad.LoadAd(requestbuilder.Build());
+			return ad;
+		}
+	}
+}
