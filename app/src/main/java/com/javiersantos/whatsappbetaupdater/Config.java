5d0f1d6932d0897a90dd3dd7f8a326be0df628a7
Javier Santos <me@javiersantos.me>
2016-01-16 01:52:20 +0100

    Update GitHub url

--- app/src/main/java/com/javiersantos/whatsappbetaupdater/Config.java
+++ app/src/main/java/com/javiersantos/whatsappbetaupdater/Config.java
@@ -2,7 +2,7 @@ package com.javiersantos.whatsappbetaupdater;
 
 public class Config {
 
-    public static final String GITHUB_URL = "https://github.com/javiersantos/WhatsAppBetaUpdater.Android";
+    public static final String GITHUB_URL = "https://github.com/javiersantos/WhatsAppBetaUpdater";
     public static final String GITHUB_TAGS = GITHUB_URL.concat("/tags");
     public static final String GITHUB_APK = GITHUB_URL.concat("/releases/download/");
     public static final String WHATSAPP_URL = "http://www.whatsapp.com/android/";
